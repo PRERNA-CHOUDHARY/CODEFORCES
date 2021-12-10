56cbadbf406033788657add8ca01b6b006d39ea5
Prerna Choudhary <74724592+PRERNA-CHOUDHARY@users.noreply.github.com>
2021-12-10 19:26:17 +0530

    Create Bear and Big Brother.cpp

---
+++ Bear and Big Brother.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c=0;
+    cin>>a>>b;
+    while(a<=b)
+    {
+        a*=3;
+        b*=2;
+        c++;
+    }
+    cout<<c;
+    return 0;
+}
