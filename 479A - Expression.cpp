a3c154e24c25456dc42dc872855865b0f25c5e12
Prerna Choudhary <74724592+PRERNA-CHOUDHARY@users.noreply.github.com>
2021-12-31 21:53:36 +0530

    479A - Expression

---
+++ 479A - Expression.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    int x[6];
+     x[0]=a*(b+c);
+     x[1]=(a*b)+c;
+     x[2]=a*b*c;
+     x[3]=(a+b)*c;
+     x[4]=a+b+c;
+     x[5]=a+(b*c);
+    sort(x,x+6);
+    cout<<x[5];
+    
+  
+
+    return 0;
+}
