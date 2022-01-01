be1bc2ce0670a50ea0175eaa14d827af8d52b053
Prerna Choudhary <74724592+PRERNA-CHOUDHARY@users.noreply.github.com>
2022-01-01 22:42:17 +0530

    466A - Cheap Travel

---
+++ Div2/466A - Cheap Travel.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n,m,a,b;
+  cin>>n>>m>>a>>b;
+  if(m*a<=b)
+  cout<<n*a;
+  else 
+  cout<<(n/m*b)+min(n%m*a,b);
+    return 0;
+}
